65913550764b5211b9e4b45576041b80a304985b
JacobJ215 <jacob.johnson2718@gmail.com>
2021-06-29 23:24:07 -0400

    v2

--- wallet.py
+++ wallet.py
@@ -1,6 +1,7 @@
 # Import dependencies
 import subprocess
 import json
+import os
 from dotenv import load_dotenv
 
 # Load and set environment variables
@@ -11,29 +12,83 @@ mnemonic=os.getenv("mnemonic")
 # YOUR CODE HERE
 
 from constants import *
+from bit import Key, PrivateKey, PrivateKeyTestnet
+from bit.network import NetworkAPI
+from bit import *
+from web3 import Web3
+from eth_account import Account 
+
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1.8545"))
+
+from web3.middleware import geth_poa_middleware
+w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
 
  
- 
-# Create a function called `derive_wallets`
-def derive_wallets(# YOUR CODE HERE):
-    command = # YOUR CODE HERE
+# Create a function called `derive_wallets'
+def derive_wallets(mnemonic, coin, numderive):
+    command = f'./derive -g --mnemonic="{mnemonic}" --numderive="{numderive}" --coin="{coin}" --format=json'
     p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     output, err = p.communicate()
     p_status = p.wait()
     return json.loads(output)
 
 # Create a dictionary object called coins to store the output from `derive_wallets`.
-coins = # YOUR CODE HERE
+coins = {"eth", "btc-test", "btc"}
+numderive = 3
+    
+# Setting the dictionarry
+keys = {}
+for coin in coins:
+    keys[coin]= derive_wallets(os.getenv('mnemonic'), coin, numderive=3)
+
+eth_PrivateKey = keys["eth"][0]['privkey']
+btc_PrivateKey = keys['btc-test'][0]['privkey']
+
+
+print(json.dumps(eth_PrivateKey, indent=4, sort_keys=True))
+print(json.dumps(btc_PrivateKey, indent=4, sort_keys=True))
+print(json.dumps(keys, indent=4, sort_keys=True))
 
 # Create a function called `priv_key_to_account` that converts privkey strings to account objects.
-def priv_key_to_account(# YOUR CODE HERE):
-    # YOUR CODE HERE
+def priv_key_to_account(coin, priv_key):
+    if coin == ETH:
+        return Account.privateKeyToAccount(priv_key)
+    if coin == BTCTEST:
+        return PrivateKeyTestnet(priv_key)
+    
+eth_acc = priv_key_to_account(ETH,eth_PrivateKey)
+btc_acc = priv_key_to_account(BTCTEST,btc_PrivateKey)
+
 
 # Create a function called `create_tx` that creates an unsigned transaction appropriate metadata.
-def create_tx(# YOUR CODE HERE):
-    # YOUR CODE HERE
+def create_tx(coin, account, recipient, amount):
+    if coin == ETH: 
+        gasEstimate = w3.eth.estimateGas(
+            {"from":eth_acc.address, "to":recipient, "value": amount}
+        )
+        return { 
+            "from": eth_acc.address,
+            "to": recipient,
+            "value": amount,
+            "gasPrice": w3.eth.gasPrice,
+            "gas": gasEstimate,
+            "nonce": w3.eth.getTransactionCount(eth_acc.address)
+        }
+    
+    elif coin == BTCTEST:
+        return PrivateKeyTestnet.prepare_transaction(account.address, [(recipient, amount, BTC)])    
+    
 
 # Create a function called `send_tx` that calls `create_tx`, signs and sends the transaction.
-def send_tx(# YOUR CODE HERE):
-    # YOUR CODE HERE
-
+def send_tx(coin,account,recipient, amount):
+    if coin == "eth": 
+        tx_eth = create_tx(coin,account, recipient, amount)
+        sign_tx = account.signTransaction(tx_eth)
+        result = w3.eth.sendRawTransaction(sign_tx.rawTransaction)
+        print(result.hex())
+        return result.hex()
+    elif coin == BTCTEST:
+        tx_btctest= create_tx(coin,account,recipient,amount)
+        sign_tx = account.sign_transaction(tx_btctest)      
+        return NetworkAPI.broadcast_tx_testnet(sign_tx)   
